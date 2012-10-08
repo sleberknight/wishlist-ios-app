1b0ce0a5e5eeb3424dbf1f06e18aae2784090620
Scott Leberknight <scott.leberknight@nearinfinity.com>
2012-10-08 14:05:40 -0400

    Added WLItemStore class header/impl files

---
+++ Wishlist/WLItemStore.m
@@ -0,0 +1,13 @@
+//
+//  WLItemStore.m
+//  Wishlist
+//
+//  Created by Scott Leberknight on 10/8/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import "WLItemStore.h"
+
+@implementation WLItemStore
+
+@end
