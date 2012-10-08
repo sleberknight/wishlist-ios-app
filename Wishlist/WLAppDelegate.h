964e28d299ad99bd64777b65a62fb1a75c17cec9
Scott Leberknight <scott.leberknight@nearinfinity.com>
2012-10-08 12:51:48 -0400

    Initial Commit

---
+++ Wishlist/WLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WLAppDelegate.h
+//  Wishlist
+//
+//  Created by Scott Leberknight on 10/8/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
