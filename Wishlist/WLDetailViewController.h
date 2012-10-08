57f9731a2d084e5613cb575f2b0bd5fbfe5efc50
Scott Leberknight <scott.leberknight@nearinfinity.com>
2012-10-08 14:58:12 -0400

    Add WLDetailViewController with XIB for user interface. Is empty now

---
+++ Wishlist/WLDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLDetailViewController.h
+//  Wishlist
+//
+//  Created by Scott Leberknight on 10/8/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLDetailViewController : UIViewController
+
+@end
---
+++ Wishlist/WLDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  WLDetailViewController.m
+//  Wishlist
+//
+//  Created by Scott Leberknight on 10/8/12.
+//  Copyright (c) 2012 Scott Leberknight. All rights reserved.
+//
+
+#import "WLDetailViewController.h"
+
+@interface WLDetailViewController ()
+
+@end
+
+@implementation WLDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
